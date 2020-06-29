4f6f1a8482f43653742bbccb2d542c8e4017389c
ZiadAli <ziadali147@gmail.com>
2020-06-29 18:56:08 -0400

    Added hangman class

--- src/App.js
+++ src/App.js
@@ -1,13 +1,4 @@
 import React from "react"
-import logo from "./logo.svg"
-
-import hangman1 from "./images/hangman1.png"
-import hangman2 from "./images/hangman2.png"
-import hangman3 from "./images/hangman3.png"
-import hangman4 from "./images/hangman4.png"
-import hangman5 from "./images/hangman5.png"
-import hangman6 from "./images/hangman6.png"
-import hangman7 from "./images/hangman7.png"
 
 import "./App.css"
 
@@ -21,17 +12,19 @@ class App extends React.Component {
 	}
 
 	handleTextChange(event) {
+		//This function is called whenever the text in the text field is modified
 		alert(event.target.value)
 	}
 
 	handleSubmit(event) {
+		//This function is called whenever the user clicks "submit" - process the text that was entered here
 		alert("Submitted")
 	}
 
 	render() {
 		return (
 			<div className="App">
-				<img className="hangman" src={hangman1} alt="hangman1" />
+				{/*Insert code to render a Hangman component here and pass it information to display the right image*/}
 				<form onSubmit={this.handleSubmit}>
 					<input
 						type="text"
---
+++ src/Hangman.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+import hangman1 from "./images/hangman1.png"
+import hangman2 from "./images/hangman2.png"
+import hangman3 from "./images/hangman3.png"
+import hangman4 from "./images/hangman4.png"
+import hangman5 from "./images/hangman5.png"
+import hangman6 from "./images/hangman6.png"
+import hangman7 from "./images/hangman7.png"
